092813a8a4c33ac81a996c58dfc1cb6906b14d31
Khammessi Ram <thekhammessi@gmail.com>
2021-04-20 14:12:44 +0100

    Initial commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/main", name="main")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route  ("/", name="home")
+     */
+
+    public function home():Response{
+        return $this->render('main/home.html.twig',
+            [
+                'title'=>"Bienvenue ici les amis",
+                'age'=>31
+            ]
+        );
+    }
+}
